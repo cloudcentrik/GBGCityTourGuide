ff12ee2106bb7c41e83f0197b77220fa2b4cb6f6
info.cloudcentrik <info.cloudcentrik@gmail.com>
2016-06-06 19:43:55 +0200

    All Hotels activities added

---
+++ app/src/main/java/net/cloudcentrik/gbgcitytourguide/RadissonBlueHotelActivity.java
@@ -0,0 +1,12 @@
+package net.cloudcentrik.gbgcitytourguide;
+
+/**
+ * Created by ismail on 21/05/16.
+ */
+public class RadissonBlueHotelActivity extends BaseActivity {
+
+    public RadissonBlueHotelActivity(){
+        super("RadissonBlueHotelActivity",R.layout.base_layout,StringsUtils.getTemValues());
+    }
+
+}
