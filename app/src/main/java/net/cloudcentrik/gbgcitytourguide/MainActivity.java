3cffb16aa3246d04a0b16eaf3dfd8ffe5a8401b2
info.cloudcentrik <info.cloudcentrik@gmail.com>
2016-06-11 21:12:57 +0200

    Toolbar style added

--- app/src/main/java/net/cloudcentrik/gbgcitytourguide/MainActivity.java
+++ app/src/main/java/net/cloudcentrik/gbgcitytourguide/MainActivity.java
@@ -1,6 +1,8 @@
 package net.cloudcentrik.gbgcitytourguide;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
