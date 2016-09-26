d57f47f1cafa7c8630558ab10825406ee6dd769f
info.cloudcentrik <info.cloudcentrik@gmail.com>
2016-09-26 19:09:35 +0200

    Halal Restaurants activity updated

---
+++ app/src/main/java/net/cloudcentrik/gbgcitytourguide/DivanHalalRestaurantActivity.java
@@ -0,0 +1,12 @@
+package net.cloudcentrik.gbgcitytourguide;
+
+/**
+ * Created by ismail on 21/05/16.
+ */
+public class DivanHalalRestaurantActivity extends BaseActivity {
+
+    public DivanHalalRestaurantActivity(){
+        super("DivanHalalRestaurantActivity",R.layout.base_layout,StringsUtils.getTemValues());
+    }
+
+}
---
+++ app/src/main/java/net/cloudcentrik/gbgcitytourguide/SarayHalalRestaurantActivity.java
@@ -0,0 +1,12 @@
+package net.cloudcentrik.gbgcitytourguide;
+
+/**
+ * Created by ismail on 21/05/16.
+ */
+public class SarayHalalRestaurantActivity extends BaseActivity {
+
+    public SarayHalalRestaurantActivity(){
+        super("SarayHalalRestaurantActivity",R.layout.base_layout,StringsUtils.getTemValues());
+    }
+
+}
