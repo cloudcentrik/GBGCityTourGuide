e4978aca7c38dea52bc7caeb92b126553505bf56
info.cloudcentrik <info.cloudcentrik@gmail.com>
2016-10-26 00:47:28 +0200

    Bas map fragment updated

--- app/src/main/java/net/cloudcentrik/gbgcitytourguide/BaseGoogleMapFragment.java
+++ app/src/main/java/net/cloudcentrik/gbgcitytourguide/BaseGoogleMapFragment.java
@@ -4,15 +4,21 @@ package net.cloudcentrik.gbgcitytourguide;
  * Created by Packard Bell on 2016-04-04.
  */
 
+import android.*;
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
+import android.support.v7.app.AppCompatActivity;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -20,11 +26,12 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class BaseGoogleMapFragment extends Fragment implements OnMapReadyCallback{
+public class BaseGoogleMapFragment extends Fragment implements OnMapReadyCallback,ActivityCompat.OnRequestPermissionsResultCallback{
 
     private String mapInfo;
 
@@ -32,6 +39,9 @@ public class BaseGoogleMapFragment extends Fragment implements OnMapReadyCallbac
     private Bundle mBundle;
 
     private Double lad,lan;
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    private boolean mPermissionDenied = false;
 
     public static BaseGoogleMapFragment newInstance(String address, String contactInfo, String mapId) {
         BaseGoogleMapFragment fragment = new BaseGoogleMapFragment();
@@ -76,13 +86,16 @@ public class BaseGoogleMapFragment extends Fragment implements OnMapReadyCallbac
         LatLng marker = new LatLng(lad,lan);
         map.addMarker(new MarkerOptions()
                 .position(marker)
-                .title("Burgerking")
-                .snippet("Centralstation")
+                .title(getActivity().getTitle().toString())
+                .snippet(getArguments().getString("ADDRESS",""))
                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
         );
 
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(marker,15));
+        map.getUiSettings().setZoomControlsEnabled(true);
+        map.getUiSettings().setCompassEnabled(true);
     }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
